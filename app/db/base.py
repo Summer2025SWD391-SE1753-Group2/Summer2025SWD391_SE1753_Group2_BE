ab83383d157bd8aafd06e72287cb6c6be800fd6a
nhducminhqt <nhducminhqt@gmail.com>
2025-05-30 16:05:59 +0700

    ref(DB): config for neon.tech and migration

---
+++ app/db/base.py
@@ -0,0 +1,3 @@
+from app.db.models.account import Account
+from app.db.models.role import Role
+from app.db.base_class import Base
--- app/db/base_class.py
+++ app/db/base_class.py
@@ -5,7 +5,6 @@ class Base:
     id: any
     __name__: str
 
-    # Generate table name automatically
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
