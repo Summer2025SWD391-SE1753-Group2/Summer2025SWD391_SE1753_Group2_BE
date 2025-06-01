8d956d95e49b654237b7aa56b12381f688a362e0
nhducminhqt <nhducminhqt@gmail.com>
2025-06-01 13:19:44 +0700

    ref(BE): refractor code using base model

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,4 +1,7 @@
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import Column, DateTime, String
+from datetime import datetime, timezone
+
 
 @as_declarative()
 class Base:
@@ -8,3 +11,7 @@ class Base:
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+    created_by = Column(String(100), nullable=True)
+    updated_by = Column(String(100), nullable=True)
