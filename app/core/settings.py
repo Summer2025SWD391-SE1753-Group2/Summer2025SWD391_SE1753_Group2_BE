2ba2fe03af1766f758ff9f7a3b344389c259f442
nhducminhqt <nhducminhqt@gmail.com>
2025-06-05 16:05:33 +0700

    chore(BE):add URL google call back

--- app/core/settings.py
+++ app/core/settings.py
@@ -32,7 +32,7 @@ class Settings(BaseSettings):
     # Google OAuth settings
     CLIENT_ID: str
     CLIENT_SECRET: str
-    GOOGLE_REDIRECT_URI: str = "http://localhost:5173/auth/google-callback"
+    GOOGLE_REDIRECT_URI: str = "http://localhost:8000/api/v1/auth/google/callback"
     FRONTEND_URL: str = "http://localhost:5173"
 
 
